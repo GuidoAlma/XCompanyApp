4531e7ad3a334966db07c6acbfeb868507f3e8e7
Guido Alma <guido.alma@live.com.ar>
2019-12-13 10:28:58 -0300

    Creación modelos

---
+++ XCompanyApp/XCompanyApp/Models/Permisos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace XCompanyApp.Models
+{
+    public class Permisos
+    {       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string NombreEmpleado { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string ApellidosEmpleado { get; set; }
+
+        [Required]
+        public DateTime FechaPermiso { get; set; }
+
+        [Required]
+        [ForeignKey("TipoPermisoRef")]
+        public int TipoPermiso { get; set; }
+        
+        TipoPermiso TipoPermisoRef { get; set; }
+    }
+}
